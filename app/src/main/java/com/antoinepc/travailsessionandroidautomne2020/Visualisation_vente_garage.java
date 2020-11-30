a667534f77147fd796b91864cd278876edf0ce6e
mAntoineSt <antoine.professionnal@gmail.com>
2020-11-30 13:33:48 -0500

    Repushage V2

---
+++ app/src/main/java/com/antoinepc/travailsessionandroidautomne2020/Visualisation_vente_garage.java
@@ -0,0 +1,14 @@
+package com.antoinepc.travailsessionandroidautomne2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Visualisation_vente_garage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualisation_vente_garage);
+    }
+}
